7fe821f4a3c0e31d3f548b5a47e58b0a175ecdf8
tesfahiwet <atesfahiwetasmelash@gmail.com>
2018-06-04 02:43:10 -0500

    Lab_6

---
+++ grade.js
@@ -0,0 +1,17 @@
+//'use strict';
+
+class Grade{
+
+    Constructor(){
+        this.gadeList = [];
+    }
+
+    find
+
+    add(obj){
+        gradeList.push(obj)
+    }
+
+}
+
+module.exports = Grade;
